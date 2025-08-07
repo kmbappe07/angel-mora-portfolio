b1eb8695d1ab1db7cfb0013aea5c7c5577323ba6
kmbappe07 <mora.fabian@icloud.com>
2025-08-06 19:55:31 -0500

    updated projects section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,12 +1,6 @@
 import { RevealOnScroll } from "../RevealOnScroll"
 
 export const Projects = () => {
-    const backendSkills = [
-        "Python", 
-        "Django", 
-        "C", 
-        "Java", 
-    ];
 
     return (
         <section 
@@ -20,12 +14,12 @@ export const Projects = () => {
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                        <h3 className="text-xl font-bold mb-2">Facebook</h3>
+                        <h3 className="text-xl font-bold mb-2">Portfolio</h3>
                         <p className="text-gray-400 mb-4">
-                            Collaborated with Mark Zuckberg to make Facebook
+                            Created a personal portfolio website
                         </p>
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js"].map((tech, key) => (
+                            {["React", "Vite"].map((tech, key) => (
                                 <span
                                         key={key}
                                         className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
@@ -38,7 +32,8 @@ export const Projects = () => {
                         
                         <div className="flex justify-between items-center">
                             <a 
-                            href="#https://github.com/redrupm/msoe-baja/blob/gh-pages/robots.txt" 
+                            href="https://github.com/moraangel/angel-mora-portfolio"
+                            target="_blank" 
                             className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                             >
                                 View Project →
@@ -46,59 +41,6 @@ export const Projects = () => {
                         </div>
                     </div>
 
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                        <h3 className="text-xl font-bold mb-2">Facebook</h3>
-                        <p className="text-gray-400 mb-4">
-                            Collaborated with Mark Zuckberg to make Facebook
-                        </p>
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js"].map((tech, key) => (
-                                <span
-                                        key={key}
-                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
-                                >
-                                    {tech}
-                                </span>
-                        ))}
-                        </div>
-                        
-                        <div className="flex justify-between items-center">
-                            <a 
-                            href="#https://github.com/redrupm/msoe-baja/blob/gh-pages/robots.txt" 
-                            className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                            >
-                                View Project →
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                        <h3 className="text-xl font-bold mb-2">Facebook</h3>
-                        <p className="text-gray-400 mb-4">
-                            Collaborated with Mark Zuckberg to make Facebook
-                        </p>
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js"].map((tech, key) => (
-                                <span
-                                        key={key}
-                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
-                                >
-                                    {tech}
-                                </span>
-                        ))}
-                        </div>
-                        
-                        <div className="flex justify-between items-center">
-                            <a 
-                            href="#https://github.com/redrupm/msoe-baja/blob/gh-pages/robots.txt" 
-                            className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                            >
-                                View Project →
-                            </a>
-                        </div>
-                    </div>
 
                 </div>
             </div>
